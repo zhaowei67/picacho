5a08a763ce920a4f01351296033bdb4701e89ff9
zhaowei67 <nicolezhao67@163.com>
2019-01-02 19:09:32 +0800

    add speed-control button

--- main.js
+++ main.js
@@ -1,18 +1,38 @@
 !function(){
+    var duration = 50
+    $('.actions').on('click','button',function(e){
+        let $button = $(e.currentTarget)
+        let speed = $button.attr('data-speed')
+        console.log(speed)
+        $button.addClass('active')
+            .siblings('.active').removeClass('active')
+        switch(speed){
+            case 'slow':
+                duration = 100
+                break
+            case 'normal':
+                duration = 50
+                break
+            case 'fast':
+                duration = 10
+                break
+        }
+    })
     function writeCode(prefix, code, fn){
         let container = document.querySelector('#code')
         let styleTag = document.querySelector('.styleTag')
         let n =0
-        let id = setInterval(()=>{
+        let id = setTimeout(function run(){
             n+=1
-            container.innerText = code.substring(0,n)
-            styleTag.innerText = code.substring(0,n)
+            container.innerHTML = code.substring(0,n)
+            styleTag.innerHTML = code.substring(0,n)
             container.scrollTop = container.scrollHeight
-            if(n >= code.length) {
-                clearInterval(id)
+            if(n < code.length) {
+                id =setTimeout(run,duration)
+            }else {
                 fn && fn.call()
             }
-        },10 )
+        },duration )
     }
     let code = `
 /*
@@ -175,10 +195,7 @@
 */
     `
     writeCode('',code)
+
+
 }.call()
 
-$('.actions').on('click','button',function(e){
-    let $button = $(e.currentTarget)
-    let speed = $button.attr('data-speed')
-    console.log(speed)
-})
